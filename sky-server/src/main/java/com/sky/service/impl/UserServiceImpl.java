0a9512aea751daeb0ff63c341aecda9e75ccaabd
24840 <2484047829@qq.com>
2023-07-24 22:55:07 +0800

    商品业务浏览功能业务代码

---
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -0,0 +1,76 @@
+package com.sky.service.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.sky.constant.MessageConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.exception.LoginFailedException;
+import com.sky.mapper.UserMapper;
+import com.sky.properties.WeChatProperties;
+import com.sky.service.UserService;
+import com.sky.utils.HttpClientUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private static final String WX_LOGIN = "https://api.weixin.qq.com/sns/jscode2session";
+
+    @Autowired
+    private WeChatProperties weChatProperties;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User wxLogin(UserLoginDTO userLoginDTO) {
+
+        //通过微信接口获取openid
+        String openid = getOpenid(userLoginDTO.getCode());
+
+
+        //判断openid是否为null，如果为null则抛出异常
+        if (openid == null) {
+            throw new LoginFailedException(MessageConstant.LOGIN_FAILED);
+        }
+
+        //查询该openid是否为新用户
+        User user = userMapper.select(openid);
+
+        //如果是新用户，则注册到user表中
+        if (user == null){
+            user = User.builder()
+                    .openid(openid)
+                    .createTime(LocalDateTime.now())
+                    .build();
+            userMapper.insert(user);
+        }
+
+        //返回user对象
+        return user;
+    }
+
+    /**
+     * 调用微信接口获取当前微信的id
+     * @param code
+     * @return
+     */
+    private String getOpenid(String code){
+        Map<String, String> map = new HashMap<>();
+        map.put("appid",weChatProperties.getAppid());
+        map.put("secret",weChatProperties.getSecret());
+        map.put("js_code",code);
+        map.put("grant_type","authorization_code");
+        String json = HttpClientUtil.doGet(WX_LOGIN, map);
+
+        JSONObject jsonObject = JSONObject.parseObject(json);
+        String openid = jsonObject.getString("openid");
+        return openid;
+    }
+}
